92116d6433cc6d08a90aa9e439a3ff64829745a1
34308 <34308@student.pwsztar.edu.pl>
2023-04-16 16:47:25 +0200

    czysczenie

--- DjangoDB/views.py
+++ DjangoDB/views.py
@@ -10,6 +10,7 @@ import io
 import base64
 from django.http import JsonResponse
 
+
 def get_database():
     # Provide the mongodb atlas url to connect python to mongodb using pymongo
     CONNECTION_STRING = "mongodb+srv://User2:QIHSchL8W0aQ5Uqb@bigdatadatabase.vatkkvd.mongodb.net/?retryWrites=true&w=majority"
@@ -22,7 +23,6 @@ def get_database():
 
 
 def ListOfCountriesWichWeHaveDataOn(request):
-
     response = requests.get("https://api.covid19api.com/summary")
     data = response.json()
     countries = pd.DataFrame(data["Countries"])
@@ -42,7 +42,7 @@ def ListOfCountriesWichWeHaveDataOn(request):
         collection_name = dbname["ListOfCountries"]
         # print(cursor[0]["_id"])
         # update
-        collection_name.update_one({"_id":cursor[0]["_id"]},{'$set': {"data": data_dict}})
+        collection_name.update_one({"_id": cursor[0]["_id"]}, {'$set': {"data": data_dict}})
     else:
         collection_name = dbname["ListOfCountries"]
         # new Table
@@ -50,22 +50,6 @@ def ListOfCountriesWichWeHaveDataOn(request):
 
     return JsonResponse(data_dict, safe=False)
 
-# def covidPlot(request):
-#     url = "https://api.covid19api.com/total/dayone/country/poland"
-#     data = requests.get(url).json()
-#     df = pd.json_normalize(data)
-#
-#     fig, ax = plt.subplots()
-#     ax.plot(df["Date"], df["Confirmed"])
-#     ax.set_title("COVID-19 Confirmed Cases in Poland")
-#     ax.set_xlabel("Date")
-#     ax.set_ylabel("Confirmed Cases")
-#
-#     buffer = io.BytesIO()
-#     plt.savefig(buffer, format='png')
-#     plt.close(fig)
-#
-#     return HttpResponse(buffer.getvalue(), content_type="image/png")
 
 def covidPlot(request, country):
     url = f'https://api.covid19api.com/total/dayone/country/{country}'
@@ -75,7 +59,7 @@ def covidPlot(request, country):
 
     fig, ax = plt.subplots()
     ax.plot(df["Date"], df["Confirmed"])
-    ax.set_title("COVID-19 Confirmed Cases in "+country)
+    ax.set_title("COVID-19 Confirmed Cases in " + country)
     ax.set_xlabel("Date")
     ax.set_ylabel("Confirmed Cases")
 
